117c65dc8c4b33764ed0cbd7e32cee9337ddb37a
Brittany Morris <brittmmendez@gmail.com>
2017-11-30 20:45:22 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
 array=string.split(" ")
-array.collect do |word|
-  word.reverse
-  word.join(" ")
-  end
+rev=[]
+array.each do |word|
+rev<<word.reverse
+end
+rev.join(" ")
 end
